6e356b25fb2122da2156b7101b761a168e9047fb
EY4180 <tundra.sand7024@eagereverest.com>
2026-03-21 09:16:19 +0800

    Transitioning to Osmosis Plugin

---
+++ src/main/java/org/villseriol/osmosis/kakasi/App.java
@@ -0,0 +1,9 @@
+// This software is released into the Public Domain.  See copying.txt for details.
+package org.villseriol.osmosis.kakasi;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+}
---
+++ src/test/java/org/villseriol/osmosis/kakasi/AppTest.java
@@ -0,0 +1,14 @@
+// This software is released into the Public Domain.  See copying.txt for details.
+package org.villseriol.osmosis.kakasi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
